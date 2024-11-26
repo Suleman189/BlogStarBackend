96ced3e7a8c1eff4ceec83cf0fdd0eed9a98f5f4
Suleman <suleman.bscs.189@gmail.com>
2024-11-26 06:36:30 +0500

    Added Project Gray Structure

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
-
+import HomeRoutes from './Routes/Home.js'
 
 const app = express();
+
+app.use(express.json())
+app.use('/home', HomeRoutes)
 
 app.get('/', (req, res) => {
   res.json({message: 'I am awesome'})
