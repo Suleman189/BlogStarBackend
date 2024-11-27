f840572a0c8982f2fe7c408ec19c6d7f9082a264
Suleman <suleman.bscs.189@gmail.com>
2024-11-27 07:20:19 +0500

    Added User Controller for authentication

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import HomeRoutes from './Routes/Home.js'
+import UserRoutes from './Routes/UserRoutes.js'
 import connectDB from './Configs/db.js';
+import expressListEndpoints from 'express-list-endpoints';
 import 'dotenv/config'
 
 connectDB();
@@ -9,6 +11,7 @@ const app = express();
 // console.log(`Port from env file is ${process.env.PORT}`)
 const PORT = process.env.PORT
 app.use(express.json())
+app.use('/api', UserRoutes)
 app.use('/home', HomeRoutes)
 
 app.get('/', (req, res) => {
@@ -16,6 +19,7 @@ app.get('/', (req, res) => {
 })
 
 
+console.log(expressListEndpoints(app))
 app.listen(PORT, () => {
   console.log(`Started Listening on the server at ${PORT}`)
 })
