072d1ccb3e5404ebc71855b4dbcf4c0ea3dabfc5
Suleman <suleman.bscs.189@gmail.com>
2024-11-28 10:45:10 +0500

    Added initial script for node project console

---
+++ repl.js
@@ -0,0 +1,30 @@
+import userModel from "./Models/user.model.js";
+import repl from 'repl';
+import fs from 'fs';
+import path from 'path';
+import mongoose from "mongoose";
+const server = repl.start('> ');
+server.context.fs = fs;
+server.context.path = path;
+import connectDB from "./Configs/db.js";
+import 'dotenv/config'
+
+server.context.UserModel = userModel;
+connectDB();
+
+
+// Handle REPL exit
+server.on('exit', async () => {
+  console.log('Closing MongoDB connection...');
+  await mongoose.disconnect();
+  console.log('MongoDB connection closed. Goodbye! 99');
+  process.exit(0); // Exit the process
+});
+
+// Alternatively, handle process signals (e.g., CTRL+C)
+process.on('SIGINT', async () => {
+  console.log('\nSIGINT received. Closing MongoDB connection...');
+  await mongoose.disconnect();
+  console.log('MongoDB connection closed. Goodbye!  55');
+  process.exit(0);
+});
