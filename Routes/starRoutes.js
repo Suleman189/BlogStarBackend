25b2e812f90c54719255555bf697a44bbbde9e70
Suleman <suleman.bscs.189@gmail.com>
2024-12-11 14:37:40 +0500

    Code improvement

--- Routes/starRoutes.js
+++ Routes/starRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createStar, getAllStars } from '../Controllers/StarsController.js';
+import { authenticateJWT } from '../Middlewares/JwtAuthentication.js';
 const router = express.Router();
 
-router.get('/', getAllStars)
-router.post('/', createStar)
+router.get('/', authenticateJWT, getAllStars);
+router.post('/', authenticateJWT, createStar);
 
 export default router;
